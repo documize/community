51f934a0ee9c579f674751e764456888979dbcb6
zinyando <brizdigital@gmail.com>
2016-05-13 19:35:10 +0200

    Add appMeta model and getSessionItem method

--- app/tests/helpers/stub-session.js
+++ app/tests/helpers/stub-session.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
+import Models from 'documize/utils/model';
 
 const Session = Ember.Service.extend({
+    appMeta: Ember.computed(function(){
+        return Models.AppMeta.create();
+    }),
     login(credentials) {
         // TODO: figure out what to do with credentials
         return new Ember.RSVP.resolve();
+    },
+
+    boot(){
+        return new Ember.RSVP.resolve();
+    },
+    getSessionItem(key){
+        return this.get(`data.${key}`);
     }
 });
 
