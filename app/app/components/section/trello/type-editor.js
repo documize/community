20e70f52d0eb30e21afeba4bdc50fd2e14bdb6ae
Harvey Kandola <harvey@documize.com>
2016-05-15 18:44:12 -0700

    trello smart section config handling

--- app/app/components/section/trello/type-editor.js
+++ app/app/components/section/trello/type-editor.js
@@ -8,21 +8,34 @@ export default Ember.Component.extend(NotifierMixin, TooltipMixin, {
     isDirty: false,
     waiting: false,
     authenticated: false,
-    config: {
-        appKey: "",
-        board: null,
-        list: null,
-        cards: null
-    },
+    config: {},
     boards: null,
     lists: null,
 
-    hasAppKey: function() {
-        console.log(is.not.empty(this.get('config.appKey')));
-        return is.empty(this.get('config.appKey')) ? false : true;
-    }.property('config'),
+    didReceiveAttrs() {
+        let config = {};
 
-    didReceiveAttrs() {},
+        try {
+            config = JSON.parse(this.get('meta.config'));
+        } catch (e) {}
+
+        if (is.empty(config)) {
+            config = {
+                appKey: "",
+                token: "",
+                board: null,
+                list: null,
+                cards: null
+            };
+        }
+
+        this.set('config', config);
+
+        if (this.get('config.appKey') !== "" &&
+            this.get('config.token') !== "") {
+            this.send('auth');
+        }
+    },
 
     willDestroyElement() {
         this.destroyTooltips();
@@ -30,30 +43,52 @@ export default Ember.Component.extend(NotifierMixin, TooltipMixin, {
 
     getBoardLists() {
         let self = this;
+        let boards = this.get('boards');
         let board = this.get('config.board');
+        this.set('waiting', true);
+
+        if (board === null) {
+            if (boards.length) {
+                board = boards[0];
+                this.set('config.board', board);
+            }
+        } else {
+            this.set('config.board', boards.findBy('id', board.id));
+        }
 
         Trello.get(`boards/${board.id}/lists/open`,
             function(lists) {
-                self.set('lists', lists);
-
-                if (lists.length) {
-                    self.set('config.list', lists[0]);
+                let savedLists = self.get('config.lists');
+                if (savedLists === null) {
+                    savedLists = [];
                 }
 
-                self.getListCards();
+                lists.forEach(function(list) {
+                    let saved = savedLists.findBy("id", list.id);
+                    let included = true;
+                    if (is.not.undefined(saved)) {
+                        included = saved.included;
+                    }
+                    list.included = included;
+                });
+
+                self.set('config.lists', lists);
+                self.set('waiting', false);
+
+                // self.getListCards();
             });
     },
 
-    getListCards() {
-        let self = this;
-        let list = this.get('config.list');
+    // getListCards() {
+    //     let self = this;
+    //     let list = this.get('config.list');
 
-        Trello.get(`lists/${list.id}/cards`,
-            function(cards) {
-                self.set('config.cards', cards);
-                console.log(cards);
-            });
-    },
+    //     Trello.get(`lists/${list.id}/cards`,
+    //         function(cards) {
+    //             self.set('config.cards', cards);
+    //             console.log(cards);
+    //         });
+    // },
 
     actions: {
         isDirty() {
@@ -64,18 +99,29 @@ export default Ember.Component.extend(NotifierMixin, TooltipMixin, {
             window.open("https://trello.com/app-key", "Trello App Key", "");
         },
 
-        getAuthToken() {
-            let appKey = this.get('config.appKey');
-            let self = this;
+        onListCheckbox(id) {
+            let lists = this.get('config.lists');
+            let list = lists.findBy('id', id);
 
-            if (is.empty(appKey)) {
-                $("#trello-appkey").addClass("error").focus();
+            if (list !== null) {
+                Ember.set(list, 'included', !list.included);
+            }
+        },
+
+        auth() {
+            if (this.get('config.appKey') === "") {
+                $("#trello-appkey").addClass('error').focus();
+                this.set('authenticated', false);
                 return;
             }
 
-            Ember.$.getScript("https://api.trello.com/1/client.js?key=" + appKey, function() {
+            let self = this;
+            self.set('waiting', true);
+
+            Ember.$.getScript("https://api.trello.com/1/client.js?key=" + this.get('config.appKey'), function() {
                 Trello.authorize({
                     type: "popup",
+                    // interactive: false,
                     name: "Documize",
                     scope: {
                         read: true,
@@ -85,22 +131,16 @@ export default Ember.Component.extend(NotifierMixin, TooltipMixin, {
                     persist: true,
                     success: function() {
                         self.set('authenticated', true);
+                        self.set('config.token', Trello.token());
                         Trello.get("members/me/boards?fields=id,name,url,closed,prefs,idOrganization",
                             function(boards) {
+                                self.set('waiting', false);
                                 self.set('boards', boards.filterBy("closed", false));
-                                let board = boards.length ? boards[0] : null;
-                                self.set('config.board', board);
                                 self.getBoardLists();
                             });
-
-                        Trello.get("members/me/organizations",
-                            function(orgs) {
-                                self.set('orgs', orgs);
-                                console.log(orgs);
-                            });
-
                     },
                     error: function(error) {
+                        self.set('waiting', false);
                         self.showNotification("Unable to authenticate");
                         console.log(error);
                     }
@@ -109,14 +149,16 @@ export default Ember.Component.extend(NotifierMixin, TooltipMixin, {
         },
 
         onBoardChange(board) {
+            this.set('isDirty', true);
             this.set('config.board', board);
+            this.set('config.lists', []);
             this.getBoardLists();
         },
 
-        onListChange(list) {
-            this.set('config.list', list);
-            this.getListCards();
-        },
+        // onListChange(list) {
+        //     this.set('config.list', list);
+        //     this.getListCards();
+        // },
 
         onCancel() {
             this.attrs.onCancel();
@@ -126,6 +168,10 @@ export default Ember.Component.extend(NotifierMixin, TooltipMixin, {
             let page = this.get('page');
             let meta = this.get('meta');
             page.set('title', title);
+            // meta.set('rawBody', JSON.stringify(this.get("items")));
+            meta.set('rawBody', '');
+            meta.set('config', JSON.stringify(this.get('config')));
+            meta.set('externalSource', true);
 
             this.attrs.onAction(page, meta);
         }
