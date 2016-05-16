0a40c968490c1369f79b37fb46b34d109b358a97
Elliott Stoneham <elliott.stoneham@gmail.com>
2016-05-16 10:23:06 +0100

    Improve nil check in utility.Close

--- wordsmith/utility/defclose.go
+++ wordsmith/utility/defclose.go
@@ -4,8 +4,12 @@ import "io"
 import "github.com/documize/community/wordsmith/log"
 
 // Close is a convenience function to close an io.Closer, usually in a defer.
-func Close(f io.Closer) {
-	if f != nil && f != io.Closer(nil) {
-		log.IfErr(f.Close())
+func Close(f interface{}) {
+	if f != nil {
+		if ff, ok := f.(io.Closer); ok {
+			if ff != io.Closer(nil) {
+				log.IfErr(ff.Close())
+			}
+		}
 	}
 }
