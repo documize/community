1d29caef53381b44f22b1260ca8f6747feec6829
Harvey Kandola <harvey@documize.com>
2016-05-19 16:09:07 -0700

    change test url back to localhost

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,11 +1,11 @@
 // Copyright 2016 Documize Inc. <legal@documize.com>. All rights reserved.
 //
-// This software (Documize Community Edition) is licensed under 
+// This software (Documize Community Edition) is licensed under
 // GNU AGPL v3 http://www.gnu.org/licenses/agpl-3.0.en.html
 //
 // You can operate outside the AGPL restrictions by purchasing
 // Documize Enterprise Edition and obtaining a commercial license
-// by contacting <sales@documize.com>. 
+// by contacting <sales@documize.com>.
 //
 // https://documize.com
 
@@ -33,7 +33,7 @@ module.exports = function(environment) {
         ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
 
         ENV.apiHost = "https://localhost:5001";
-        ENV.apiHost = "https://demo1.dev:5001";
+        // ENV.apiHost = "https://demo1.dev:5001";
     }
 
     if (environment === 'test') {
@@ -49,7 +49,8 @@ module.exports = function(environment) {
         // ENV.locationType = 'none';
         // ENV.APP.rootElement = '#ember-testing';
 
-        ENV.apiHost = "https://demo1.dev:5001";
+        ENV.apiHost = "https://localhost:5001";
+        // ENV.apiHost = "https://demo1.dev:5001";
     }
 
     if (environment === 'production') {
@@ -63,7 +64,6 @@ module.exports = function(environment) {
     }
 
     ENV.apiNamespace = "api";
-
     ENV.contentSecurityPolicy = null;
 
     // ENV.contentSecurityPolicy = {
