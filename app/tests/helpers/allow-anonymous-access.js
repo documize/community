57a1e30df7b55af4bfcf1bd61abde3dc1bea3e10
zinyando <brizdigital@gmail.com>
2016-05-30 20:32:24 +0200

    WIP helper for tests with allow anon access set to true

---
+++ app/tests/helpers/allow-anonymous-access.js
@@ -0,0 +1,6 @@
+// import Ember from 'ember';
+//
+// export default Ember.Test.registerAsyncHelper('allowAnonymousAccess', function(app) {
+//     var sessionService = Ember.getOwner(this).lookup("service:session");
+//     console.log(sessionService);
+// });
