b840236cb5bbed5049ca13d08dea39f977965f40
Harvey Kandola <harvey@documize.com>
2016-06-01 16:16:44 +0100

    fixed height issue for markdown preview window

--- app/app/components/section/markdown/type-editor.js
+++ app/app/components/section/markdown/type-editor.js
@@ -10,7 +10,7 @@ export default Ember.Component.extend({
 
     didInsertElement() {
         let height = $(document).height() - $(".document-editor > .toolbar").height() - 130;
-        $("#markdown-editor, #markdown-preview").css("height", height);
+        $("#markdown-editor").css("height", height);
 
         this.renderPreview();
         let self = this;
