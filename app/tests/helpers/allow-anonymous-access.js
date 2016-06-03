97d2bd2a0ea1c564244ceed1b8120b6acedee331
zinyando <brizdigital@gmail.com>
2016-06-03 13:41:57 +0200

    Delete unused helper

--- app/tests/helpers/allow-anonymous-access.js
+++
@@ -1,6 +0,0 @@
-// import Ember from 'ember';
-//
-// export default Ember.Test.registerAsyncHelper('allowAnonymousAccess', function(app) {
-//     var sessionService = Ember.getOwner(this).lookup("service:session");
-//     console.log(sessionService);
-// });
