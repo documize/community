f22a4a582b43c5e3b36a4a8beb74b2aa8050f5fb
zinyando <brizdigital@gmail.com>
2016-06-14 16:11:22 +0200

    Uncomment not-found route in router

--- app/app/router.js
+++ app/app/router.js
@@ -1,11 +1,11 @@
 // Copyright 2016 Documize Inc. <legal@documize.com>. All rights reserved.
 //
-// This software (Documize Community Edition) is licensed under 
+// This software (Documize Community Edition) is licensed under
 // GNU AGPL v3 http://www.gnu.org/licenses/agpl-3.0.en.html
 //
 // You can operate outside the AGPL restrictions by purchasing
 // Documize Enterprise Edition and obtaining a commercial license
-// by contacting <sales@documize.com>. 
+// by contacting <sales@documize.com>.
 //
 // https://documize.com
 
@@ -94,9 +94,9 @@ export default Router.map(function() {
         path: 'widgets'
     });
 
-    // this.route('not-found', {
-    //     path: '/*wildcard'
-    // });
+    this.route('not-found', {
+        path: '/*wildcard'
+    });
 
     this.route('pods', function() {});
 });
