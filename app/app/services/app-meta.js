e25fb2dce5da91f23bf17d77fb16758648dfe6f9
Harvey Kandola <harvey@documize.com>
2016-06-25 10:37:44 -0700

    Create app-meta.js

---
+++ app/app/services/app-meta.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+import config from '../config/environment';
+
+const {
+	String: { htmlSafe },
+	RSVP: { resolve },
+	inject: { service }
+} = Ember;
+
+export default Ember.Service.extend({
+	ajax: service(),
+
+	url: `${config.apiHost}/${config.apiNamespace}`,
+	orgId: '',
+	title: '',
+	version: '',
+	message: '',
+	allowAnonymousAccess: false,
+
+	boot() {
+		let dbhash;
+		if (is.not.null(document.head.querySelector("[property=dbhash]"))) {
+			dbhash = document.head.querySelector("[property=dbhash]").content;
+		}
+
+		let isInSetupMode = dbhash && dbhash !== "{{.DBhash}}";
+		if (isInSetupMode) {
+			this.setProperites({
+				title: htmlSafe("Documize Setup"),
+				allowAnonymousAccess: false
+			});
+			return resolve();
+		}
+
+		return this.get('ajax').request('public/meta')
+	        .then((response) => {
+				this.setProperties(response);
+	        });
+	}
+});
