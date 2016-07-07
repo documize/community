f397e9406b1e378e944f7a102d4279a96a1b5f09
zinyando <brizdigital@gmail.com>
2016-07-07 17:40:17 +0200

    Fix jshint errors

--- app/app/services/document.js
+++ app/app/services/document.js
@@ -13,250 +13,250 @@ import Ember from 'ember';
 import models from '../utils/model';
 
 export default Ember.Service.extend({
-    sessionService: Ember.inject.service('session'),
-    ajax: Ember.inject.service(),
+	sessionService: Ember.inject.service('session'),
+	ajax: Ember.inject.service(),
 
-    // Returns document model for specified document id.
-    getDocument(documentId) {
-        return this.get('ajax').request(`documents/${documentId}`, {
-            method: "GET"
-        }).then((response) => {
-            return models.DocumentModel.create(response);
-        });
-    },
+	// Returns document model for specified document id.
+	getDocument(documentId) {
+		return this.get('ajax').request(`documents/${documentId}`, {
+			method: "GET"
+		}).then((response) => {
+			return models.DocumentModel.create(response);
+		});
+	},
 
-    // Returns all documents for specified folder.
-    getAllByFolder(folderId) {
-        return this.get('ajax').request(`documents?folder=${folderId}`, {
-            method: "GET"
-        }).then((response) => {
-            let documents = Ember.ArrayProxy.create({
-                content: Ember.A([])
-            });
+	// Returns all documents for specified folder.
+	getAllByFolder(folderId) {
+		return this.get('ajax').request(`documents?folder=${folderId}`, {
+			method: "GET"
+		}).then((response) => {
+			let documents = Ember.ArrayProxy.create({
+				content: Ember.A([])
+			});
 
-            _.each(response, function(doc) {
-                let documentModel = models.DocumentModel.create(doc);
-                documents.pushObject(documentModel);
-            });
+			_.each(response, function (doc) {
+				let documentModel = models.DocumentModel.create(doc);
+				documents.pushObject(documentModel);
+			});
 
-            return documents;
-        });
-    },
+			return documents;
+		});
+	},
 
-    // getDocumentsByTag returns all documents for specified tag (not folder!).
-    getAllByTag(tag) {
-        return this.get('ajax').request(`documents?filter=tag&tag=${tag}`, {
-            method: "GET"
-        }).then((response) => {
-            let documents = Ember.ArrayProxy.create({
-                content: Ember.A([])
-            });
+	// getDocumentsByTag returns all documents for specified tag (not folder!).
+	getAllByTag(tag) {
+		return this.get('ajax').request(`documents?filter=tag&tag=${tag}`, {
+			method: "GET"
+		}).then((response) => {
+			let documents = Ember.ArrayProxy.create({
+				content: Ember.A([])
+			});
 
-            _.each(response, function(doc) {
-                let documentModel = models.DocumentModel.create(doc);
-                documents.pushObject(documentModel);
-            });
+			_.each(response, function (doc) {
+				let documentModel = models.DocumentModel.create(doc);
+				documents.pushObject(documentModel);
+			});
 
-            return documents;
-        });
-    },
+			return documents;
+		});
+	},
 
-    // saveDocument updates an existing document record.
-    save(doc) {
-        let id = doc.get('id');
+	// saveDocument updates an existing document record.
+	save(doc) {
+		let id = doc.get('id');
 
-        return this.get('ajax').request(`documents/${id}`, {
-            method: 'PUT',
-            data: JSON.stringify(doc)
-        });
-    },
+		return this.get('ajax').request(`documents/${id}`, {
+			method: 'PUT',
+			data: JSON.stringify(doc)
+		});
+	},
 
-    getBatchedPages: function(documentId, payload) {
-        let url = `documents/${documentId}/pages/batch`;
+	getBatchedPages: function (documentId, payload) {
+		let url = `documents/${documentId}/pages/batch`;
 
-        return this.get('ajax').request(url, {
-            method: 'POST',
-            data: payload
-        }).then((pages) => {
-            if (is.not.array(pages)) {
-                pages = [];
-            }
+		return this.get('ajax').request(url, {
+			method: 'POST',
+			data: payload
+		}).then((pages) => {
+			if (is.not.array(pages)) {
+				pages = [];
+			}
 
-            return pages;
-        });
-    },
+			return pages;
+		});
+	},
 
-    changePageSequence: function(documentId, payload) {
-        let url = `documents/${documentId}/pages/sequence`;
+	changePageSequence: function (documentId, payload) {
+		let url = `documents/${documentId}/pages/sequence`;
 
-        return this.get('ajax').post(url, {
-            data: JSON.stringify(payload),
-            contentType: 'json'
-        });
-    },
+		return this.get('ajax').post(url, {
+			data: JSON.stringify(payload),
+			contentType: 'json'
+		});
+	},
 
-    changePageLevel(documentId, payload) {
-        let url = `documents/${documentId}/pages/level`;
+	changePageLevel(documentId, payload) {
+		let url = `documents/${documentId}/pages/level`;
 
-        return this.get('ajax').post(url, {
-            data: JSON.stringify(payload),
-            contentType: 'json'
-        });
-    },
+		return this.get('ajax').post(url, {
+			data: JSON.stringify(payload),
+			contentType: 'json'
+		});
+	},
 
-    deleteDocument: function(documentId) {
-        let url = `documents/${documentId}`;
+	deleteDocument: function (documentId) {
+		let url = `documents/${documentId}`;
 
-        return this.get('ajax').request(url, {
-            method: 'DELETE'
-        });
-    },
+		return this.get('ajax').request(url, {
+			method: 'DELETE'
+		});
+	},
 
-    updatePage: function(documentId, pageId, payload, skipRevision) {
-        var revision = skipRevision ? "?r=true" : "?r=false";
-        let url = `documents/${documentId}/pages/${pageId}${revision}`
+	updatePage: function (documentId, pageId, payload, skipRevision) {
+		var revision = skipRevision ? "?r=true" : "?r=false";
+		let url = `documents/${documentId}/pages/${pageId}${revision}`;
 
-        return this.get('ajax').request(url, {
-            method: 'PUT',
-            data: JSON.stringify(payload),
-            contentType: 'json'
-        });
-    },
+		return this.get('ajax').request(url, {
+			method: 'PUT',
+			data: JSON.stringify(payload),
+			contentType: 'json'
+		});
+	},
 
-    // addPage inserts new page to an existing document.
-    addPage: function(documentId, payload) {
-        let url = `documents/${documentId}/pages`;
+	// addPage inserts new page to an existing document.
+	addPage: function (documentId, payload) {
+		let url = `documents/${documentId}/pages`;
 
-        return this.get('ajax').post(url, {
-            data: JSON.stringify(payload),
-            contentType: 'json'
-        });
-    },
+		return this.get('ajax').post(url, {
+			data: JSON.stringify(payload),
+			contentType: 'json'
+		});
+	},
 
-    // Nukes multiple pages from the document.
-    deletePages: function(documentId, pageId, payload) {
-        let url = `documents/${documentId}/pages/${pageId}`;
+	// Nukes multiple pages from the document.
+	deletePages: function (documentId, pageId, payload) {
+		let url = `documents/${documentId}/pages/${pageId}`;
 
-        return this.get('ajax').post(url, {
-            data: JSON.stringify(payload),
-            contentType: 'json'
-        });
-    },
+		return this.get('ajax').post(url, {
+			data: JSON.stringify(payload),
+			contentType: 'json'
+		});
+	},
 
-    // Nukes a single page from the document.
-    deletePage: function(documentId, pageId) {
-        let url = `documents/${documentId}/pages/${pageId}`;
+	// Nukes a single page from the document.
+	deletePage: function (documentId, pageId) {
+		let url = `documents/${documentId}/pages/${pageId}`;
 
-        return this.get('ajax').request(url, {
-            method: 'DELETE'
-        });
-    },
+		return this.get('ajax').request(url, {
+			method: 'DELETE'
+		});
+	},
 
-    getPageRevisions(documentId, pageId) {
-        let url = `documents/${documentId}/pages/${pageId}/revisions`;
+	getPageRevisions(documentId, pageId) {
+		let url = `documents/${documentId}/pages/${pageId}/revisions`;
 
-        return this.get('ajax').request(url, {
-            method: "GET"
-        });
-    },
+		return this.get('ajax').request(url, {
+			method: "GET"
+		});
+	},
 
-    getPageRevisionDiff(documentId, pageId, revisionId) {
-        let url = `documents/${documentId}/pages/${pageId}/revisions/${revisionId}`;
+	getPageRevisionDiff(documentId, pageId, revisionId) {
+		let url = `documents/${documentId}/pages/${pageId}/revisions/${revisionId}`;
 
-        return this.get('ajax').request(url, {
-            method: "GET",
-            dataType: 'text'
-        });
-    },
+		return this.get('ajax').request(url, {
+			method: "GET",
+			dataType: 'text'
+		});
+	},
 
-    rollbackPage(documentId, pageId, revisionId) {
-        let url = `documents/${documentId}/pages/${pageId}/revisions/${revisionId}`;
+	rollbackPage(documentId, pageId, revisionId) {
+		let url = `documents/${documentId}/pages/${pageId}/revisions/${revisionId}`;
 
-        return this.get('ajax').request(url, {
-            method: "POST"
-        });
-    },
+		return this.get('ajax').request(url, {
+			method: "POST"
+		});
+	},
 
-    // document meta referes to number of views, edits, approvals, etc.
-    getMeta(documentId) {
+	// document meta referes to number of views, edits, approvals, etc.
+	getMeta(documentId) {
 
-        return this.get('ajax').request(`documents/${documentId}/meta`, {
-            method: "GET"
-        });
-    },
+		return this.get('ajax').request(`documents/${documentId}/meta`, {
+			method: "GET"
+		});
+	},
 
-    // Returns all pages without the content
-    getTableOfContents(documentId) {
+	// Returns all pages without the content
+	getTableOfContents(documentId) {
 
-        return this.get('ajax').request(`documents/${documentId}/pages?content=0`, {
-            method: 'GET'
-        }).then((response) => {
-            let data = [];
-            _.each(response, function(obj) {
-                data.pushObject(models.PageModel.create(obj));
-            });
+		return this.get('ajax').request(`documents/${documentId}/pages?content=0`, {
+			method: 'GET'
+		}).then((response) => {
+			let data = [];
+			_.each(response, function (obj) {
+				data.pushObject(models.PageModel.create(obj));
+			});
 
-            return data;
-        });
-    },
+			return data;
+		});
+	},
 
-    // Returns all document pages with content
-    getPages(documentId) {
+	// Returns all document pages with content
+	getPages(documentId) {
 
-        return this.get('ajax').request(`documents/${documentId}/pages`, {
-            method: 'GET'
-        }).then((response) => {
-            let pages = [];
+		return this.get('ajax').request(`documents/${documentId}/pages`, {
+			method: 'GET'
+		}).then((response) => {
+			let pages = [];
 
-            _.each(response, function(page) {
-                pages.pushObject(models.PageModel.create(page));
-            });
+			_.each(response, function (page) {
+				pages.pushObject(models.PageModel.create(page));
+			});
 
-            return pages;
-        });
-    },
+			return pages;
+		});
+	},
 
-    // Returns document page with content
-    getPage(documentId, pageId) {
+	// Returns document page with content
+	getPage(documentId, pageId) {
 
-        return this.get('ajax').request(`documents/${documentId}/pages/${pageId}`, {
-            method: 'GET'
-        }).then((response) => {
-            let page = models.PageModel.create(response);
-            return page;
-        });
-    },
+		return this.get('ajax').request(`documents/${documentId}/pages/${pageId}`, {
+			method: 'GET'
+		}).then((response) => {
+			let page = models.PageModel.create(response);
+			return page;
+		});
+	},
 
-    // Returns document page meta object
-    getPageMeta(documentId, pageId) {
+	// Returns document page meta object
+	getPageMeta(documentId, pageId) {
 
-        return this.get('ajax').request(`documents/${documentId}/pages/${pageId}/meta`, {
-            method: 'GET'
-        }).then((response) => {
-            let meta = models.PageMetaModel.create(response);
-            return meta;
-        });
-    },
+		return this.get('ajax').request(`documents/${documentId}/pages/${pageId}/meta`, {
+			method: 'GET'
+		}).then((response) => {
+			let meta = models.PageMetaModel.create(response);
+			return meta;
+		});
+	},
 
-    // document attachments without the actual content
-    getAttachments(documentId) {
+	// document attachments without the actual content
+	getAttachments(documentId) {
 
-        return this.get('ajax').request(`documents/${documentId}/attachments`, {
-            method: 'GET'
-        }).then((response) => {
-            let data = [];
-            _.each(response, function(obj) {
-                data.pushObject(models.AttachmentModel.create(obj));
-            });
-            return data;
-        });
-    },
+		return this.get('ajax').request(`documents/${documentId}/attachments`, {
+			method: 'GET'
+		}).then((response) => {
+			let data = [];
+			_.each(response, function (obj) {
+				data.pushObject(models.AttachmentModel.create(obj));
+			});
+			return data;
+		});
+	},
 
-    // nuke an attachment
-    deleteAttachment(documentId, attachmentId) {
+	// nuke an attachment
+	deleteAttachment(documentId, attachmentId) {
 
-        return this.get('ajax').request(`documents/${documentId}/attachments/${attachmentId}`, {
-            method: 'DELETE'
-        });
-    },
+		return this.get('ajax').request(`documents/${documentId}/attachments/${attachmentId}`, {
+			method: 'DELETE'
+		});
+	},
 });
