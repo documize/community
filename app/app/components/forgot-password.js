1eff7613369252f3bae5f27d9869010b10c3f64b
zinyando <brizdigital@gmail.com>
2016-07-26 14:49:47 +0200

    WIP refactor forgot password route to use a component

---
+++ app/app/components/forgot-password.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+const {
+	computed,
+	isEmpty
+} = Ember;
+
+export default Ember.Component.extend({
+	email: "",
+	sayThanks: false,
+	emptyEmail: computed('email', 'emptyEmailError', {
+		get() {
+			if (isEmpty(this.get('email')) && this.get('emptyEmailError')) {
+				return `error`;
+			}
+
+			return;
+		}
+	}),
+
+	actions: {
+		forgot() {
+			let email = this.get('email');
+
+			if (isEmpty(email)) {
+				Ember.set(this, 'emptyEmailError', true);
+				return $("#email").focus();
+			}
+
+			this.get('forgot')(email).then(() => {
+				Ember.set(this, 'sayThanks', true);
+				Ember.set(this, 'email', '');
+			}).catch((error) => {
+				let message = error.message;
+			});
+		}
+	}
+});
