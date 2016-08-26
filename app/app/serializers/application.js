19f77855fd90c34620aa8a77517458fa940c0f70
zinyando <brizdigital@gmail.com>
2016-08-26 13:02:54 +0200

    Tweak format of returned data

--- app/app/serializers/application.js
+++ app/app/serializers/application.js
@@ -3,9 +3,11 @@ import JSONAPISerializer from 'ember-data/serializers/json-api';
 export default JSONAPISerializer.extend({
 	normalize(modelClass, resourceHash) {
 		return {
-			id: resourceHash.id,
-			type: modelClass.modelName,
-			attributes: resourceHash
+			data: {
+				id: resourceHash.id,
+				type: modelClass.modelName,
+				attributes: resourceHash
+			}
 		};
 	}
 });
--- app/app/serializers/page.js
+++ app/app/serializers/page.js
@@ -3,9 +3,11 @@ import ApplicationSerializer from './application';
 export default ApplicationSerializer.extend({
 	normalize(modelClass, resourceHash) {
 		return {
-			id: resourceHash.id ? resourceHash.id : resourceHash.documentId,
-			type: modelClass.modelName,
-			attributes: resourceHash
+			data: {
+				id: resourceHash.id ? resourceHash.id : resourceHash.documentId,
+				type: modelClass.modelName,
+				attributes: resourceHash
+			}
 		};
 	}
 });
