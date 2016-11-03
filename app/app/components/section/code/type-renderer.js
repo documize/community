822dba64f9b2a06570ef23af40ea4d1798353bec
Harvey Kandola <harvey@documize.com>
2016-11-03 14:40:18 -0700

    fixed codemirror url view mode

--- app/app/components/section/code/type-renderer.js
+++ app/app/components/section/code/type-renderer.js
@@ -1,11 +1,11 @@
 // Copyright 2016 Documize Inc. <legal@documize.com>. All rights reserved.
 //
-// This software (Documize Community Edition) is licensed under 
+// This software (Documize Community Edition) is licensed under
 // GNU AGPL v3 http://www.gnu.org/licenses/agpl-3.0.en.html
 //
 // You can operate outside the AGPL restrictions by purchasing
 // Documize Enterprise Edition and obtaining a commercial license
-// by contacting <sales@documize.com>. 
+// by contacting <sales@documize.com>.
 //
 // https://documize.com
 
@@ -16,7 +16,7 @@ export default Ember.Component.extend({
     codeSyntax: "htmlmixed",
 
     didReceiveAttrs() {
-        CodeMirror.modeURL = "codemirror/mode/%N/%N.js";
+        CodeMirror.modeURL = "/codemirror/mode/%N/%N.js";
 
         let page = this.get('page');
         let rawBody = page.get('body');
