e4192caab90db2b3e3398ead5e286cc6cdab431e
Harvey Kandola <harvey@documize.com>
2017-02-21 12:11:47 -0800

    amends

---
+++ app/app/models/user-activity.js
@@ -0,0 +1,32 @@
+// Copyright 2016 Documize Inc. <legal@documize.com>. All rights reserved.
+//
+// This software (Documize Community Edition) is licensed under
+// GNU AGPL v3 http://www.gnu.org/licenses/agpl-3.0.en.html
+//
+// You can operate outside the AGPL restrictions by purchasing
+// Documize Enterprise Edition and obtaining a commercial license
+// by contacting <sales@documize.com>.
+//
+// https://documize.com
+
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import Ember from 'ember';
+
+export default Model.extend({
+	documentName: attr('string'),
+	folderId: attr('string'),
+	contributed: attr('string'),
+	viewed: attr('string'),
+	created: attr('string'),
+
+	hasContributed: Ember.computed('contributed', function () {
+		return this.get('contributed').length > 0;
+	}),
+	hasViewed: Ember.computed('viewed', function () {
+		return this.get('viewed').length > 0;
+	}),
+	hasCreated: Ember.computed('created', function () {
+		return this.get('created').length > 0;
+	})
+});
