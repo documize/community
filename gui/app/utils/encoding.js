42b86b0513a5b22b9a8484c6b1ee600ad3d4769d
Harvey Kandola <harvey@documize.com>
2017-09-05 12:21:18 +0100

    address js lint warning

--- gui/app/utils/encoding.js
+++ gui/app/utils/encoding.js
@@ -1,11 +1,11 @@
 // Copyright 2016 Documize Inc. <legal@documize.com>. All rights reserved.
 //
-// This software (Documize Community Edition) is licensed under 
+// This software (Documize Community Edition) is licensed under
 // GNU AGPL v3 http://www.gnu.org/licenses/agpl-3.0.en.html
 //
 // You can operate outside the AGPL restrictions by purchasing
 // Documize Enterprise Edition and obtaining a commercial license
-// by contacting <sales@documize.com>. 
+// by contacting <sales@documize.com>.
 //
 // https://documize.com
 
@@ -41,7 +41,7 @@ var Base64 = {
         var n, r, i;
         var s, o, u, a;
         var f = 0;
-        e = e.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+        e = e.replace(/[^A-Za-z0-9+/=]/g, "");
         while (f < e.length) {
             s = this._keyStr.indexOf(e.charAt(f++));
             o = this._keyStr.indexOf(e.charAt(f++));
--- gui/tests/.eslintrc.js
+++ gui/tests/.eslintrc.js
@@ -1,5 +1,36 @@
 module.exports = {
-  env: {
-    embertest: true
-  }
+	root: true,
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module'
+	},
+	extends: 'eslint:recommended',
+	env: {
+		browser: true,
+		jquery: true,
+		qunit: true,
+		embertest: true
+	},
+	rules: {
+	},
+	globals: {
+		"$": true,
+		"is": true,
+		"_": true,
+		"tinymce": true,
+		"CodeMirror": true,
+		"Drop": true,
+		"Mousetrap": true,
+		"Sortable": true,
+		"moment": true,
+		"Dropzone": true,
+		"Tooltip": true,
+		"server": true,
+		"authenticateUser": true,
+		"stubAudit": true,
+		"stubUserNotification": true,
+		"userLogin": true,
+		"Keycloak": true,
+		"slug": true
+	}
 };
