bffdd1f2b1e07e14dfe340fca801437786a93c66
sauls8t <saul@documize.com>
2018-01-28 17:42:24 +0000

    Ensure global admin is also default document approver

--- domain/space/endpoint.go
+++ domain/space/endpoint.go
@@ -118,7 +118,7 @@ func (h *Handler) Add(w http.ResponseWriter, r *http.Request) {
 
 	err = h.Store.Permission.AddPermissions(ctx, perm, permission.SpaceOwner, permission.SpaceManage, permission.SpaceView,
 		permission.DocumentAdd, permission.DocumentCopy, permission.DocumentDelete, permission.DocumentEdit, permission.DocumentMove,
-		permission.DocumentTemplate)
+		permission.DocumentTemplate, permission.DocumentApprove)
 	if err != nil {
 		ctx.Transaction.Rollback()
 		response.WriteServerError(w, method, err)
