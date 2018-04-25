123fce6c171a267f385f2a33c4749006a20b5a87
sauls8t <saul@documize.com>
2018-04-25 12:15:17 +0100

    Use unicode compat slug engine

--- core/stringutil/slug.go
+++ core/stringutil/slug.go
@@ -12,28 +12,10 @@
 package stringutil
 
 import (
-	"strings"
-	"unicode"
+	"github.com/documize/slug"
 )
-
-// TODO" replace with https://github.com/mcmatts/slug
 
 // MakeSlug creates a slug, suitable for use in a URL, from a string
 func MakeSlug(str string) string {
-	slg := strings.Map(
-		func(r rune) rune { // individual mapping of runes into a format suitable for use in a URL
-			r = unicode.ToLower(r)
-			if unicode.IsLower(r) || unicode.IsDigit(r) {
-				return r
-			}
-			return '-'
-		}, str)
-	slg = strings.NewReplacer("---", "-", "--", "-").Replace(slg)
-	for strings.HasSuffix(slg, "-") {
-		slg = strings.TrimSuffix(slg, "-")
-	}
-	for strings.HasPrefix(slg, "-") {
-		slg = strings.TrimPrefix(slg, "-")
-	}
-	return slg
+	return slug.Make(str)
 }
