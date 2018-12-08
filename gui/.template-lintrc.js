c7808d0b5a178f32aab7d52fb33fcce8f17f0a40
Harvey Kandola <harvey@documize.com>
2018-12-08 19:06:43 +0000

    [WIP] Upgrade EmberJS: add-on upgrades

--- gui/.template-lintrc.js
+++ gui/.template-lintrc.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+	  	'attribute-indentation': false,
+	  	'block-indentation': false,
+	  	'no-nested-interactive': false,
+		'link-rel-noopener': false,
+		'no-triple-curlies': false,
+		'style-concatenation': false,
+		'no-inline-styles': false,
+		'img-alt-attributes': false,
+  	}
 };
+
+// https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rules.md
