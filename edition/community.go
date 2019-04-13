2fffb7869e233474ee5962e0b1c04ec1782c87b8
sauls8t <saul@documize.com>
2019-04-13 17:23:37 +0100

    Bump version

--- edition/community.go
+++ edition/community.go
@@ -41,8 +41,8 @@ func main() {
 	rt.Product = domain.Product{}
 	rt.Product.Major = "2"
 	rt.Product.Minor = "3"
-	rt.Product.Patch = "0"
-	rt.Product.Revision = "190409124440"
+	rt.Product.Patch = "1"
+	rt.Product.Revision = "190411165656"
 	rt.Product.Version = fmt.Sprintf("%s.%s.%s", rt.Product.Major, rt.Product.Minor, rt.Product.Patch)
 	rt.Product.Edition = domain.CommunityEdition
 	rt.Product.Title = fmt.Sprintf("%s Edition", rt.Product.Edition)
