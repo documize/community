2c164a135a0fd7ca2dbba43db03a1e9cf24b739e
HarveyKandola <harvey@documize.com>
2019-09-05 10:37:40 +0100

    Remove obsolete build flag

--- build.sh
+++ build.sh
@@ -50,18 +50,20 @@ go generate
 
 cd ..
 echo "Compiling for Linux..."
-env GOOS=linux GOARCH=amd64 GODEBUG=tls13=1 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-linux-amd64 ./edition/community.go
+env GOOS=linux GOARCH=amd64 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-linux-amd64 ./edition/community.go
 echo "Compiling for macOS..."
-env GOOS=darwin GOARCH=amd64 GODEBUG=tls13=1 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-darwin-amd64 ./edition/community.go
+env GOOS=darwin GOARCH=amd64 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-darwin-amd64 ./edition/community.go
 echo "Compiling for Windows..."
-env GOOS=windows GOARCH=amd64 GODEBUG=tls13=1 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-windows-amd64.exe ./edition/community.go
+env GOOS=windows GOARCH=amd64 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-windows-amd64.exe ./edition/community.go
 echo "Compiling for ARM..."
-env GOOS=linux GOARCH=arm GODEBUG=tls13=1 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-linux-arm ./edition/community.go
+env GOOS=linux GOARCH=arm go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-linux-arm ./edition/community.go
 echo "Compiling for ARM64..."
-env GOOS=linux GOARCH=arm64 GODEBUG=tls13=1 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-linux-arm64 ./edition/community.go
+env GOOS=linux GOARCH=arm64 go build -gcflags="all=-trimpath=$GOPATH" -o bin/documize-community-linux-arm64 ./edition/community.go
 
 echo "Finished."
 
 # CGO_ENABLED=0 GOOS=linux go build -a -ldflags="-s -w" -installsuffix cgo
 # go build -ldflags '-d -s -w' -a -tags netgo -installsuffix netgo test.go
 # ldd test
+
+# go build -a -o main -gcflags=all=-trimpath=/home/xibz -asmflags=all=-trimpath=/home/xibz main.go
