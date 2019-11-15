d4edcb8b2c1761f0e2420fc40408b96bd8a42c6b
HarveyKandola <harvey@documize.com>
2019-11-15 16:38:56 +0000

    Show default category indicator to admins
    Fixes #339

--- core/database/scripts/sqlserver/db_00005.sql
+++ core/database/scripts/sqlserver/db_00005.sql
@@ -1,7 +1,7 @@
 /* Community edition */
 
 -- Increase column sizes to support rich text data entry
-ALTER TABLE dmz_org ALTER COLUMN c_message TYPE NVARCHAR(2000) NOT NULL DEFAULT '';
-ALTER TABLE dmz_space ALTER COLUMN c_desc TYPE NVARCHAR(2000) NOT NULL DEFAULT '';
-ALTER TABLE dmz_category ALTER COLUMN c_name TYPE NVARCHAR(200) NOT NULL DEFAULT '';
-ALTER TABLE dmz_category ADD COLUMN c_default BIT NOT NULL DEFAULT '0';
+ALTER TABLE dmz_org ALTER COLUMN c_message NVARCHAR(2000);
+ALTER TABLE dmz_space ALTER COLUMN c_desc NVARCHAR(2000);
+ALTER TABLE dmz_category ALTER COLUMN c_name NVARCHAR(200);
+ALTER TABLE dmz_category ADD c_default BIT NOT NULL DEFAULT '0';
