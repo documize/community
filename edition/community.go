cafa3ceed0351a6d83cb8d55089a142d7739b9d1
HarveyKandola <harvey@documize.com>
2019-12-06 18:02:00 +0000

    Build prep 3.5.2

--- edition/community.go
+++ edition/community.go
@@ -41,8 +41,8 @@ func main() {
 	rt.Product = domain.Product{}
 	rt.Product.Major = "3"
 	rt.Product.Minor = "5"
-	rt.Product.Patch = "1"
-	rt.Product.Revision = "191129164835"
+	rt.Product.Patch = "2"
+	rt.Product.Revision = "191206164613"
 	rt.Product.Version = fmt.Sprintf("%s.%s.%s", rt.Product.Major, rt.Product.Minor, rt.Product.Patch)
 	rt.Product.Edition = domain.CommunityEdition
 	rt.Product.Title = fmt.Sprintf("%s Edition", rt.Product.Edition)
