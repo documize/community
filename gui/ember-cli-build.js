cddba799f82450254d311f1cb32e783aabdf6812
HarveyKandola <harvey@documize.com>
2021-08-18 19:39:14 -0400

    Update Ember deps

--- gui/ember-cli-build.js
+++ gui/ember-cli-build.js
@@ -1,61 +1,55 @@
+/* eslint-disable ember/require-super-in-init */
+
 'use strict';
 
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var isDevelopment = EmberApp.env() === 'development';
-var nodeSass = require('node-sass');
-// var isTest = EmberApp.env() === 'test';
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+let isDevelopment = EmberApp.env() === 'development';
 
-module.exports = function (defaults) {
-	var app = new EmberApp(defaults, {
-		sassOptions: {
-			implementation: nodeSass
-		},
-
-		fingerprint: {
-			enabled: true,
-			generateAssetMap: true,
-			fingerprintAssetMap: true,
-			prepend: '/',
-			extensions: ['js', 'css'],
-			exclude: ['tinymce/**', 'codemirror/**', 'prism/**', 'pdfjs/**']
-		},
-
-		minifyJS: {
+module.exports = function(defaults) {
+	let app = new EmberApp(defaults, {
+		'ember-cli-terser': {
 			enabled: !isDevelopment,
-			options: {
+			exclude: ['tinymce/**', 'codemirror/**', 'prism/**', 'pdfjs/**'],
+
+			hiddenSourceMap: true,
+
+			fingerprint: {
+				enabled: true,
+				generateAssetMap: true,
+				fingerprintAssetMap: true,
+				prepend: '/',
+				extensions: ['js', 'css'],
 				exclude: ['tinymce/**', 'codemirror/**', 'prism/**', 'pdfjs/**']
-			}
-		},
+			},
 
-		minifyCSS: {
-			enabled: !isDevelopment,
-			options: {
-				exclude: ['tinymce/**', 'codemirror/**', 'prism/**', 'pdfjs/**']
-			}
-		},
+			minifyJS: {
+				enabled: !isDevelopment,
+				options: {
+					exclude: ['tinymce/**', 'codemirror/**', 'prism/**', 'pdfjs/**']
+				}
+			},
 
-		// autoprefixer: {
-		// 	sourcemap: false
-		// },
+			minifyCSS: {
+				enabled: !isDevelopment,
+				options: {
+					exclude: ['tinymce/**', 'codemirror/**', 'prism/**', 'pdfjs/**']
+				}
+			},
 
-		sourcemaps: {
-			enabled: isDevelopment,
-			extensions: ['js']
-		},
-
-		outputPaths: {
-			app: {
-				css: {
-					'app': '/assets/documize.css',
-					'themes/conference': '/assets/theme-conference.css',
-					'themes/forest': '/assets/theme-forest.css',
-					'themes/brave': '/assets/theme-brave.css',
-					'themes/harvest': '/assets/theme-harvest.css',
-					'themes/sunflower': '/assets/theme-sunflower.css',
-					'themes/silver': '/assets/theme-silver.css',
+			outputPaths: {
+				app: {
+					css: {
+						'app': '/assets/documize.css',
+						'themes/conference': '/assets/theme-conference.css',
+						'themes/forest': '/assets/theme-forest.css',
+						'themes/brave': '/assets/theme-brave.css',
+						'themes/harvest': '/assets/theme-harvest.css',
+						'themes/sunflower': '/assets/theme-sunflower.css',
+						'themes/silver': '/assets/theme-silver.css',
+					}
 				}
 			}
-		}
+		},
 	});
 
 	app.import('vendor/clipboard.js');
@@ -75,8 +69,11 @@ module.exports = function (defaults) {
 	app.import('vendor/slug.js');
 	app.import('vendor/sortable.js');
 	app.import('vendor/table-editor.min.js');
-	app.import('vendor/codemirror.js'); // core lib
-	app.import('vendor/codemirror-boot.js'); // boot-up files
+
+	// core lib
+	app.import('vendor/codemirror.js');
+	// boot-up files
+	app.import('vendor/codemirror-boot.js');
 
 	app.import('vendor/bootstrap.bundle.min.js');
 
