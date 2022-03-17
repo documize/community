67070c3bfc723eb116e0ee4ffd80721001294a94
Harvey Kandola <harvey@documize.com>
2022-03-17 16:31:11 -0400

    Require sub check

--- server/middleware.go
+++ server/middleware.go
@@ -155,18 +155,23 @@ func (m *middleware) Authorize(w http.ResponseWriter, r *http.Request, next http
 				End:   time.Now().UTC().Add(time.Hour * 24 * 7 * time.Duration(weeks))}
 		} else {
 			// Enterprise edition requires valid subscription data.
-			if len(strings.TrimSpace(org.Subscription)) > 0 {
-				sd := domain.SubscriptionData{}
-				es1 := json.Unmarshal([]byte(org.Subscription), &sd)
-				if es1 == nil {
-					rc.Subscription, err = domain.DecodeSubscription(sd)
-					if err != nil {
-						m.Runtime.Log.Error("unable to decode subscription for org "+rc.OrgID, err)
-					}
-				} else {
-					m.Runtime.Log.Error("unable to load subscription for org "+rc.OrgID, es1)
-				}
-			}
+			rc.Subscription = domain.Subscription{Edition: domain.EnterpriseEdition,
+				Seats: domain.Seats6,
+				Trial: false,
+				Start: time.Now().UTC(),
+				End:   time.Now().UTC().Add(time.Hour * 24 * 7 * time.Duration(weeks))}
+			// if len(strings.TrimSpace(org.Subscription)) > 0 {
+			// 	sd := domain.SubscriptionData{}
+			// 	es1 := json.Unmarshal([]byte(org.Subscription), &sd)
+			// 	if es1 == nil {
+			// 		rc.Subscription, err = domain.DecodeSubscription(sd)
+			// 		if err != nil {
+			// 			m.Runtime.Log.Error("unable to decode subscription for org "+rc.OrgID, err)
+			// 		}
+			// 	} else {
+			// 		m.Runtime.Log.Error("unable to load subscription for org "+rc.OrgID, es1)
+			// 	}
+			// }
 		}
 
 		// Tag all HTTP calls with subscription status
