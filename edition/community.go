1c09771c334c472f391a1fe2a96ef1a68577a36f
Harvey Kandola <harvey@documize.com>
2023-02-06 11:46:10 -0500

    Bump version

--- edition/community.go
+++ edition/community.go
@@ -40,9 +40,9 @@ func main() {
 	// Specify the product edition.
 	rt.Product = domain.Product{}
 	rt.Product.Major = "5"
-	rt.Product.Minor = "4"
-	rt.Product.Patch = "2"
-	rt.Product.Revision = "221021105923"
+	rt.Product.Minor = "5"
+	rt.Product.Patch = "0"
+	rt.Product.Revision = "230206112518"
 	rt.Product.Version = fmt.Sprintf("%s.%s.%s", rt.Product.Major, rt.Product.Minor, rt.Product.Patch)
 	rt.Product.Edition = domain.CommunityEdition
 	rt.Product.Title = "Community"
